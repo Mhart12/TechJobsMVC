f93b0db118ea220a675a932c70895f297841aa23
Monica Hart <mhart2012@gmail.com>
2018-02-12 15:43:22 -0600

    Solving Search 'All' Issue

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using TechJobs.Models;
+using System;
 
 namespace TechJobs.Controllers
 {
@@ -13,21 +14,32 @@ namespace TechJobs.Controllers
             return View();
         }
 
+        // TODO #1 - Create a Results action method to process 
+        // search request and display results
         public IActionResult Results(string searchType, string searchTerm)
         {
-            ViewBag.title = "Search";
-            ViewBag.searchTerm = searchTerm;
             ViewBag.columns = ListController.columnChoices;
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
 
-            if (searchType == "all")
+            if (String.IsNullOrEmpty(searchTerm))
             {
-                ViewBag.jobs = JobData.FindByValue(searchTerm);
+                return View("Index");
+            }
+
+            if (searchType.Equals("all"))
+            {
+                jobs = JobData.FindByValue(searchTerm);
+                ViewBag.jobs = jobs;
+                return View("Index");
             }
             else
             {
-                ViewBag.jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.jobs = jobs;
+                return View("Index");
             }
-            return View("Index");
+
         }
+
     }
 }
